7f224af19485b89c9c0a6e6c4d1a7f26a323a512
noark9 <noark9@gmail.com>
2014-12-28 23:51:00 +0800

    remove pod libraries.

--- Pods/Target Support Files/Pods-FRPlayground-ReactiveViewModel/Pods-FRPlayground-ReactiveViewModel-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FRPlayground_ReactiveViewModel : NSObject
-@end
-@implementation PodsDummy_Pods_FRPlayground_ReactiveViewModel
-@end
--- Pods/Target Support Files/Pods-FRPlayground/Pods-FRPlayground-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FRPlayground : NSObject
-@end
-@implementation PodsDummy_Pods_FRPlayground
-@end
